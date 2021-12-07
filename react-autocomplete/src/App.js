36f85dbe94357e0323c52fb93ddeae969a3b3ea2
DilrubaJi <91086050+DilrubaJi@users.noreply.github.com>
2021-12-07 20:53:50 +0000

    completed tutorial

---
+++ react-autocomplete/src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './custom.css';
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [text, setText] = useState('');
+  const [suggestions, setSuggestions] = useState([])
+  useEffect(() => {
+    const loadUsers = async () => {
+      const response = await axios.get('https://reqres.in/api/users');
+
+      setUsers(response.data.data)
+
+    }
+    loadUsers();
+
+  }, [])
+  const onSuggestHandler = (test) => {
+    setText(test);
+    setSuggestions([]);
+  }
+
+  const onChangeHandler = (text) => {
+    let matches = []
+    if (text.length > 0) {
+      matches = users.filter(user => {
+        const regex = new RegExp(`${text}`, "gi");
+        return user.email.match(regex)
+      })
+    }
+    console.log('matches', matches)
+    setSuggestions(matches)
+    setText(text)
+  }
+  return (
+    <div className="container">
+      <div>{text}</div>
+      <input type="text" className="col-md-12" styles={{ marginTop: 10 }}
+        onChange={e => onChangeHandler(e.target.value)}
+        value={text}
+        onBlur={() =>{
+          setTimeout(() => {
+            setSuggestions([])
+          }, 100);
+        }}
+      />
+      {suggestions && suggestions.map((suggestion, i) =>
+        <div key={i} className="suggestion col-md-12 justify-content-md-center"
+          onClick={() => onSuggestHandler(suggestion.email)}
+          >{suggestion.email}</div>
+      )}
+    </div>
+  );
+}
+
+export default App;
